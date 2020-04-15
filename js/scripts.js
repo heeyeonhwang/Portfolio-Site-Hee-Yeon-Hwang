0cd9710b3eb76d33196e8312576898c8af9e6a76
heeyeonhwang <heeyeonhwang@users.noreply.github.com>
2020-04-15 02:05:35 -0700

    Add toggle menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 
 console.log("Hellow world");
+
+function menuToggle(){
+  var x= document.getElementById('myNavtoggle');
+  if (x.className==='navtoggle'){
+    x.className+='responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
